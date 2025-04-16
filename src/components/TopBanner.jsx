26b12d5c8ca6886d499334a8abe4ac5c3a0e0a05
saittd03 <saittd03@gmail.com>
2025-04-16 16:57:05 +0530

    Create TopBanner.jsx

---
+++ src/components/TopBanner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopBanner = () => {
+  return <img src="/top-banner.jpg" alt="Top Banner" className="w-full h-40 object-cover" />;
+};
+
+export default TopBanner;
