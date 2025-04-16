858a58b297689e0422b07f1c86c3a612e0b84610
saittd03 <saittd03@gmail.com>
2025-04-16 19:47:58 +0530

    Update FirstPage.jsx

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -6,13 +6,21 @@ import { motion } from 'framer-motion';
 
 const FirstPage = () => {
   const navigate = useNavigate();
-  const images = Array.from({ length: 7 }, (_, i) => `/sevakImages/sevak${i + 1}.jpg`);
+  const images = [
+    'https://tirumalatirupatiyatra.in/wp-content/uploads/2024/07/Malayappa-Swamy-Saalakatla-Brahmotsavams-and-Navaratri-Brahmotsavams.jpg',
+    'https://i.pinimg.com/736x/7a/33/f3/7a33f36196448a5d160327689537e6b8.jpg',
+    'https://icctmemphis.org/wp-content/uploads/2024/10/Sesha_Vahanam.jpg',
+    'https://templesofsouthindia.wordpress.com/wp-content/uploads/2013/06/kalpavriksha-vahanam-2012.jpg?w=584',
+    'https://icctmemphis.org/wp-content/uploads/2024/10/Hanumath_Vahanam.jpg',
+    'https://icctmemphis.org/wp-content/uploads/2024/10/Hamsa_Vahanam.jpg',
+    'https://tirumalatirupatiyatra.in/wp-content/uploads/2017/09/Gaja-Vahanam.jpg',
+  ];
 
   return (
     <div>
       <TopBanner />
       <AudioPlayer />
-      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url(/https://education.sakshi.com/sites/default/files/2022-05/ttd.jpg)' }}>
+      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url(https://education.sakshi.com/sites/default/files/2022-05/ttd.jpg)' }}>
         <div className="flex justify-center items-center flex-wrap gap-4 p-4 animate-pulse">
           {images.map((img, idx) => (
             <motion.img
